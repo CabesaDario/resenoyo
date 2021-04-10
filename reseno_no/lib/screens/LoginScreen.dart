eaedc7a624faca9d1d7dee87f4605daa1f484685
DaríoOjeda <darioojeda_1995@hotmail.com>
2021-04-10 11:37:08 +0200

    Pantalla de Login funcional
    He usao el dart package flutter_login: ^1.1.0 para implementar una pantalla de login con animaciones en el cambio del login al sigin, https://pub.dev/packages/flutter_login falta usar firebase para comprobar los usuarios y mejorar el diseño, presumiblemente un backgroudn

---
+++ reseno_no/lib/screens/LoginScreen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:reseno_no/screens/HomeScreen.dart';
+
+const users = const {
+  'cabesa@gmail.com': '2232',
+  'otro@gmail.com': 'puff',
+};
+
+class LoginScreen extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+  Future<String> _authUser(LoginData data) {
+    print('Nombre: ${data.name}, Pass: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'El usuario indicado no existe';
+      }
+      if (users[data.name] != data.password) {
+        return 'La contraseña indicada no existe';
+      }
+      return null;
+    });
+  }
+
+  Future<String> _recoverPassword(String name) {
+    print('Nombre: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'El nombre de usuario indicado no existe';
+      }
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      title: 'ReseñoÑo',
+      logo: 'assets/images/ecorp-lightblue.png',
+      onLogin: _authUser,
+      onSignup: _authUser,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => HomeScreen(),
+        ));
+      },
+      onRecoverPassword: _recoverPassword,
+    );
+  }
+}
