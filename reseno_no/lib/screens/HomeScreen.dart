3fdc806fd10d4f43dd1faf1cb957b170bda6717a
DaríoOjeda <darioojeda_1995@hotmail.com>
2021-04-14 12:51:34 +0200

    comienzo a montar un swiper de carteles de peliculas
    Usando un Swiper Card widget de flutter packages comienzo a ensamblar lo que mostrará las películas populares, he añadido al manifiesto de android la línea android:usesCleartextTraffic="true", esto es porque las conexiones http inseguras estan bloqueadas por defecto en android e ios, pero necesito sacar network images para probar el swiper de momento. https://flutter.dev/docs/release/breaking-changes/network-policy-ios-android

--- reseno_no/lib/screens/HomeScreen.dart
+++ reseno_no/lib/screens/HomeScreen.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Container(
+          child: (Column(
+            children: [
+              _swiperPeliculas(),
+            ],
+          )),
+        ));
   }
 }
+
+  Widget _swiperPeliculas() {
+    return Container(
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "http://via.placeholder.com/350x150", fit: BoxFit.fill,
+          );
+        },
+
+        itemCount: 3,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+      ),
+    );
+
+  }
