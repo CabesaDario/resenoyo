0d20ed5ff05eb584410ae58ad08f09c41a3477e6
DaríoOjeda <darioojeda_1995@hotmail.com>
2021-04-26 16:49:05 +0200

    Cambio a VS e implementación populares
    Se muestran las pelis en cines en el swiper y se muestran las populares en un mini swiper horizontal.

--- reseno_no/lib/providers/peliculas_provider.dart
+++ reseno_no/lib/providers/peliculas_provider.dart
@@ -2,45 +2,31 @@ import 'dart:convert';
 
 import 'package:reseno_no/models/peliculas_model.dart';
 import 'package:http/http.dart' as http;
-class PeliculasProvider {
 
+class PeliculasProvider {
   String _apiKey = '586f0be8c98f62a6b9d5645c14e521df';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
 
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
+    final respuesta = await http.get(url);
+    final decodedData = json.decode(respuesta.body);
+    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+
+    return peliculas.items;
+  }
+
   Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language});
 
-    final url = Uri.https(_url, '3/movie/now_playing', {
-      'api_key' : _apiKey,
-      'language' : _language
-    });
-
-    final respuesta = await http.get( url );
-    final decodedData = json.decode(respuesta.body);
-    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
-
-    return peliculas.items;
-
-
+    return await _procesarRespuesta(url);
   }
+
   Future<List<Pelicula>> getPopulares() async {
+    final url = Uri.https(
+        _url, '3/movie/popular', {'api_key': _apiKey, 'language': _language});
 
-    final url = Uri.https(_url, '3/movie/popular', {
-      'api_key' : _apiKey,
-      'language' : _language
-    });
-
-    final respuesta = await http.get( url );
-    final decodedData = json.decode(respuesta.body);
-    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
-
-    return peliculas.items;
-
-
+    return await _procesarRespuesta(url);
   }
-
-
-
-
-
 }
--- reseno_no/lib/screens/HomeScreen.dart
+++ reseno_no/lib/screens/HomeScreen.dart
@@ -2,16 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:reseno_no/providers/peliculas_provider.dart';
 import 'package:reseno_no/widgets/card_swiper_widget.dart';
+import 'package:reseno_no/widgets/movie_horizontal.dart';
 
 class HomeScreen extends StatelessWidget {
   final peliculasProvider = PeliculasProvider();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Populares'),
-        leading: Container(),
-      ),
+        appBar: AppBar(
+          title: Text('Películas en cines'),
+          leading: Container(),
+        ),
         body: Container(
           child: (Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -22,33 +23,46 @@ class HomeScreen extends StatelessWidget {
           )),
         ));
   }
+
   Widget _swiperPeliculas() {
     return FutureBuilder(
         future: peliculasProvider.getEnCines(),
-        builder : (BuildContext context, AsyncSnapshot<List> snapshot){
-          if(snapshot.hasData){
+        builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+          if (snapshot.hasData) {
             return CardSwiper(peliculas: snapshot.data);
-          }else{
-            return Container(height: 400.0, child:
-                              Center(child:
-                                   CircularProgressIndicator()));
+          } else {
+            return Container(
+                height: 400.0,
+                child: Center(child: CircularProgressIndicator()));
           }
-        }
-    );
-
+        });
   }
+
   Widget _footer(BuildContext context) {
     return Container(
-      width: double.infinity,
-      child: Column(
-        children: [
-          Text('Populares', style: Theme.of(context).textTheme.subtitle1,),
-
-        ],
-      )
-    );
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text(
+                'Populares',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+            ),
+            SizedBox(height: 6.0),
+            FutureBuilder(
+              future: peliculasProvider.getPopulares(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  return MovieHorizontal(peliculas: snapshot.data);
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ],
+        ));
   }
 }
-
-
-
